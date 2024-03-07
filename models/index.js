41bc2e88c3d609d416f8cc2173046fb3a4eaf19e
Josh <josh.mayfield45@gmail.com>
2024-03-06 20:20:08 -0500

    created collaborate model and associations between models

--- models/index.js
+++ models/index.js
@@ -1,9 +1,28 @@
 // import dependencies
 const User = require('./User');
 const Product = require('./Product');
+const Collaborate = require('./Collaborate');
+
+// associations
+User.belongsToMany(Product, {
+  through: {
+    model: Collaborate,
+    unique: false,
+  },
+  as: 'collaborations'
+});
+
+Product.belongsToMany(User, {
+  through: {
+    model: Collaborate,
+    unique: false,
+  },
+  as: 'collaborators'
+});
 
 // export User
 module.exports = { 
     User,
-    Product 
+    Product,
+    Collaborate 
 };
